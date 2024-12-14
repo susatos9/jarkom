9b8d372a7dfcda1d66377b0cc6650a39fe975884
Nugroho Adi Susanto <susatos9@gmail.com>
2024-12-14 11:35:16 +0700

    Add nugi

---
+++ nugi/quiz.java
@@ -0,0 +1,26 @@
+import utils.*;
+import java.util.*;
+import java.io.*;
+import java.net.*;
+
+class quiz{
+  question[] questions;
+  user[] users;
+}
+
+class user{
+  int id,score;
+  string name;
+  String[] answer;
+}
+
+class question{
+  int id;
+  string question;
+  char answer;
+}
+
+
+static void main(String[] args){
+  System.out.println("Hello World");
+}
