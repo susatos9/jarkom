0101159b718b46079dafe60286c62c3f0c61341f
david <davidneilleenirvinne@mail.ugm.ac.id>
2024-12-18 21:14:58 +0700

    add basic function: communication

---
+++ david/Client.java
@@ -0,0 +1,94 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client{
+  private Socket socket;
+  private BufferedReader bufferedReader;
+  private BufferedWriter bufferedWriter;
+
+  private String username;
+
+  public Client(Socket socket, String username){
+    try {
+      this.socket = socket;
+      this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+      this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+      this.username = username;
+    }
+    catch(IOException e){
+      closeEverything(socket, bufferedReader, bufferedWriter);
+    }
+  }
+
+  public void listenMessageFromHost(){
+    new Thread(new Runnable() {
+      @Override
+      public void run(){
+        String msg;
+        while(socket.isConnected()){
+          try {
+            msg = bufferedReader.readLine();
+            System.out.println(msg);
+          }
+          catch(IOException e){
+            closeEverything(socket, bufferedReader, bufferedWriter);
+          }
+        }
+      }
+    }).start();
+  }
+
+  public void sendMessageToHost(){
+    try {
+      // send username
+      bufferedWriter.write(username);
+      bufferedWriter.newLine();
+      bufferedWriter.flush();
+
+      // send host status
+      bufferedWriter.write("0");
+      bufferedWriter.newLine();
+      bufferedWriter.flush();
+
+      // send answers
+      Scanner scanner = new Scanner(System.in);
+      while(socket.isConnected()){
+        String msg = scanner.nextLine();
+        bufferedWriter.write(username + ": " + msg);
+        bufferedWriter.newLine();
+        bufferedWriter.flush();
+      }
+    }
+    catch(IOException e){
+      closeEverything(socket, bufferedReader, bufferedWriter);
+    }
+  }
+
+  public void closeEverything(Socket socket, BufferedReader bufferedReader, BufferedWriter bufferedWriter){
+    try {
+      if(bufferedReader != null) bufferedReader.close();
+      if(bufferedWriter != null) bufferedWriter.close();
+      if(socket != null) socket.close();
+    }
+    catch (IOException e){
+      e.printStackTrace();
+    }
+  }
+
+  public static void main(String[]args) throws IOException {
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Enter you username for quiz: ");
+    String username = scanner.nextLine();
+
+    Socket socket = new Socket("localhost", 1234);
+    Client client = new Client(socket, username);
+
+    client.listenMessageFromHost();
+    client.sendMessageToHost();
+  }
+}
---
+++ david/Host.java
@@ -0,0 +1,103 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Host {
+  private Socket socket;
+  private BufferedReader bufferedReader;
+  private BufferedWriter bufferedWriter;
+  
+  private String username;
+
+  public Host(Socket socket, String username){
+    try {
+      this.socket = socket;
+      this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+      this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+      this.username = "Host["+username+"]";
+    }
+    catch (IOException e){
+      closeEverything(socket, bufferedReader, bufferedWriter);
+    }
+  }
+
+  public void listenMessage(){
+    new Thread(new Runnable() {
+      @Override
+      public void run(){
+        String msg;
+
+        while(socket.isConnected()){
+          try {
+            msg = bufferedReader.readLine();
+            System.out.println(msg);
+          }
+          catch (IOException e){
+            closeEverything(socket, bufferedReader, bufferedWriter);
+          }
+        }
+      }
+    }).start();
+  }
+
+  public void sendMessage(){
+    try {
+      // send username (host)
+      bufferedWriter.write("Host");
+      bufferedWriter.newLine();
+      bufferedWriter.flush();
+
+      // send host status
+      bufferedWriter.write("1");
+      bufferedWriter.newLine();
+      bufferedWriter.flush();
+
+      // send messages
+      Scanner scanner = new Scanner(System.in);
+      while(socket.isConnected()){
+        String msg = scanner.nextLine();
+        bufferedWriter.write(this.username + ": " + msg);
+        bufferedWriter.newLine();
+        bufferedWriter.flush();
+      }
+    }
+    catch(IOException e){
+      closeEverything(socket, bufferedReader, bufferedWriter);
+    }
+  }
+
+
+  public void closeEverything(Socket socket, BufferedReader bufferedReader, BufferedWriter bufferedWriter){
+    try {
+      if(bufferedReader != null){
+        bufferedReader.close();
+      }
+      if(bufferedWriter != null){
+        bufferedWriter.close();
+      } 
+      if(socket != null){
+        socket.close();
+      }
+    }
+    catch (IOException e){
+      e.printStackTrace();
+    }
+  }
+
+  public static void main(String[]args) throws IOException{
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("You are the host for this quiz, please input username ");
+    String username = scanner.nextLine();
+    
+
+    Socket socket = new Socket("localhost", 1234);
+    Host host = new Host(socket, username);
+
+    host.listenMessage();
+    host.sendMessage();
+  }
+}
